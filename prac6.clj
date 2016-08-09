3c2d867776adcca95bb03eafe1d982b0b524fc5a
Lacty <akira206@gmail.com>
2016-08-09 15:41:27 +0900

    update prac6
    より複雑なmapの使い方を記述

--- prac6.clj
+++ prac6.clj
@@ -8,3 +8,13 @@
 
 ; 自作関数を適用
 (map (fn [x] (* x 3)) '(1 2 3)) ; -> (3 6 9)
+
+
+(map + '(1 2 3) '(1 2 3)) ; -> (2 4 6)
+
+; iterateについては後程
+(map + '(1 2 3) (iterate inc 1)) ; -> (2 4 6)
+
+
+(map {2 "two" 3 "three"} '(1 2 3 4 5))
+  ; -> (nil "two" "three" nil nil)
