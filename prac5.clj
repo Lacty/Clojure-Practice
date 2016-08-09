3cba7bfdcd4beaae33e3eef955288fad8861cc2c
Lacty <akira206@gmail.com>
2016-08-09 15:15:54 +0900

    update prac5
    nilなどの評価について記述

--- prac5.clj
+++ prac5.clj
@@ -9,3 +9,21 @@
 (if false 1 2) ; -> 2
 
 (if (= 10 11) 1 2) ; -> 2
+
+
+(if (Boolean. false) "yes" "no") ; -> "yes"
+
+
+(defn is-under100? [num]
+  (if (< num 100) "yes" "no"))
+(is-under100? 50) ; -> "yes"
+
+
+(if 0 true false) ; -> true
+
+(if nil true false) ; -> false
+
+; vectorのnilはnilと同値ではない
+(if [nil] true false) ; -> true
+
+(if (first [nil]) true false) ; -> false
