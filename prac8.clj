43332ebefb395225d49fd8682e638fd6d9f78605
Lacty <akira206@gmail.com>
2016-08-10 14:39:51 +0900

    update prac8
    even odd によるフィルタリングを追加

--- prac8.clj
+++ prac8.clj
@@ -5,3 +5,13 @@
 
 
 (filter (fn [x] (= x "a")) '("a" "b" "c")) ; -> ("a")
+
+
+; even? 偶数であればtrue
+(filter even? '(1 2 3 4)) ; -> (2 4)
+
+; odd? 奇数であればtrue
+(filter odd? '(1 2 3 4)) ; -> (1 3)
+
+; range 0 ~ 与えた数
+(filter (fn [x] (< x 50)) (range 100)) ; -> (0 1 2 3... 49)
