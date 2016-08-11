df5d10f958011d52b8ce6765e451d4f1f3cb60c2
Lacty <akira206@gmail.com>
2016-08-11 19:11:25 +0900

    add FizzBuzz
    FizzBuzzを実装してみた

---
+++ FizzBuzz.clj
@@ -0,0 +1,16 @@
+
+; ClojureでFizzBuzzを実装してみる
+
+; FizzBuzz
+; 任意の数値に対して、3で割り切れる場合は"Fizz"を出力
+; 5で割り切れる場合は"Buzz"を出力
+; 15で割り切れる場合は"FizzBuzz"と出力
+; 上に該当したい場合は数値を出力
+
+
+(defn fizzbuzz [x] (cond (= (mod x 15) 0) (println "fizzbuzz")
+                         (= (mod x 5) 0) (println "Buzz")
+                         (= (mod x 3) 0) (println "Fizz")
+                         :else (println x)))
+
+(for [x (range 1 21)] (fizzbuzz x))
