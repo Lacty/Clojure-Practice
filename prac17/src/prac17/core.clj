5636c343a2dedbc013c8bfaa745a58f5e9b23bb9
Lacty <akira206@gmail.com>
2016-08-18 17:56:47 +0900

    update prac17/core.clj
    マウスの位置に円を描画

--- prac17/src/prac17/core.clj
+++ prac17/src/prac17/core.clj
@@ -2,17 +2,25 @@
   (:require [quil.core :refer :all]))
 
 (defn setup []
-  (smooth))
+  (smooth)
+  (set-state! :m-pos (atom [0 0])))
 
 (defn draw []
   (background 255)
   (fill 192)
-  (ellipse 100 100 30 30))
+  (ellipse 100 100 30 30)
+  (let [[x y] @(state :m-pos)]
+    (ellipse x y 40 40)))
+
+(defn mouse-moved []
+  (let [x (mouse-x) y (mouse-y)]
+    (reset! (state :m-pos) [x y])))
 
 (defsketch example
-  :title "Example"
+  :title "mouse"
   :setup setup
   :draw draw
+  :mouse-moved mouse-moved
   :size [200 200])
 
 (defn -main [& args])
