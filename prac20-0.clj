34476325ded5889ffd313a5ebbae4a44f09e866c
Lacty <akira206@gmail.com>
2016-08-19 18:12:12 +0900

    atom を使用した配列操作に挑戦

---
+++ prac20-0.clj
@@ -0,0 +1,5 @@
+
+(def pos (atom [0 0]))
+(def vel (atom [1 1]))
+
+(reset! pos (map + @pos @vel))
--- prac20/src/prac20/core.clj
+++ prac20/src/prac20/core.clj
@@ -2,12 +2,21 @@
   (:require [quil.core :refer :all]))
 
 (defn setup []
-  (smooth))
+  (smooth)
+  (no-stroke))
+
+(def pos (atom [0 0]))
+(def vel (atom [(rand -1.0 1.0)
+                (rand -1.0 1.0)]))
+
+(defn update []
+  (swap! pos (map + vel)))
 
 (defn draw []
   (background 255)
   (fill 192)
-  (ellipse 100 100 30 30))
+  (let [[x y] @pos]
+    (ellipse x y 30 30)))
 
 (defsketch example
   :title "Example"
