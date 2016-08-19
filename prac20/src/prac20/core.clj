266fe6c17eaddacb9caa662602c73c1cffe68f74
Lacty <akira206@gmail.com>
2016-08-19 18:26:28 +0900

    update prac20
    動くようにはなったけど
    updateが機能してない、ω、

--- prac20/src/prac20/core.clj
+++ prac20/src/prac20/core.clj
@@ -3,24 +3,27 @@
 
 (defn setup []
   (smooth)
-  (no-stroke))
+  (no-stroke)
+  (frame-rate 30))
 
 (def pos (atom [0 0]))
-(def vel (atom [(rand -1.0 1.0)
-                (rand -1.0 1.0)]))
+(def vel (atom [1 1]))
 
-(defn update []
-  (swap! pos (map + vel)))
+(defn update-state []
+  (reset! pos (map + @pos @vel))
+  (println "pos"))
 
 (defn draw []
   (background 255)
   (fill 192)
+  (reset! pos (map + @pos @vel))
   (let [[x y] @pos]
     (ellipse x y 30 30)))
 
 (defsketch example
   :title "Example"
   :setup setup
+  :update update-state
   :draw draw
   :size [200 200])
 
