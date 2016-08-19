117ecd193ac62b99e435aea79017b7c32e4ffdcf
Lacty <akira206@gmail.com>
2016-08-19 14:48:41 +0900

    update prac19
    key入力判定とれた

--- prac19/src/prac19/core.clj
+++ prac19/src/prac19/core.clj
@@ -2,17 +2,26 @@
   (:require [quil.core :refer :all]))
 
 (defn setup []
-  (smooth))
+  (smooth)
+  (no-stroke)
+  (set-state! :p-key (atom false)))
 
 (defn draw []
   (background 255)
   (fill 192)
-  (ellipse 100 100 30 30))
+  (ellipse (if @(state :p-key) 0 100) 100 30 30))
+
+(defn key-press []
+  (reset! (state :p-key) (if (= (key-code) (int \A)) true false))
+  
+  ; outputs
+  (println (if (= (key-code) (int \a)) true false) (raw-key) (key-code) (int \a)))
 
 (defsketch example
   :title "Example"
   :setup setup
   :draw draw
+  :key-pressed key-press
   :size [200 200])
 
 (defn -main [& args])
