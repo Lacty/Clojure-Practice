018f2bd161280fddb06037e8f03a9da8076b2bec
Lacty <akira206@gmail.com>
2016-09-08 19:34:03 +0900

    ballを作成する関数完成、あとは描画だ

--- prac21/src/prac21/core.clj
+++ prac21/src/prac21/core.clj
@@ -1,13 +1,24 @@
 (ns prac21.core
   (:require [quil.core :refer :all]))
 
+(defstruct Ball :pos :size)
+
 (defn setup []
   (smooth)
   (no-stroke)
   (frame-rate 30)
   (set-state! :balls (atom [])))
 
+(defn create-ball [pos size]
+  (struct Ball pos size))
+
+(defn increase-ball [state]
+  (swap! (state :balls) conj (create-ball [50 50] 5)))
+
 (defn draw []
+  (when (mouse-pressed?)
+    (increase-ball state))
+  
   (background 255)
   (fill 192)
   (ellipse 100 100 30 30))
