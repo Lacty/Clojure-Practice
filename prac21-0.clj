3d2cc24c6ff69a6abcb7ed6c157e59276c4762c6
Lacty <akira206@gmail.com>
2016-09-08 18:02:37 +0900

    配列って重複できん・・・？

--- prac21-0.clj
+++ prac21-0.clj
@@ -1,3 +1,12 @@
 
-(defstruct foo :name :hp)
-(struct foo 10 30)
+; create struct
+(defstruct foo :speed :size)
+(def ball (struct foo 2 5))
+
+; create an array to store the structure
+(def arr [])
+
+; add struct to array
+(conj arr ball)
+(conj arr ball)
+(conj arr ball)
