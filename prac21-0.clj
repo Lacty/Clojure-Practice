6b2e343653253135ad24d1c9f9620642eca6fc57
Lacty <akira206@gmail.com>
2016-09-08 19:04:16 +0900

    配列に構造体の追加が出来た

--- prac21-0.clj
+++ prac21-0.clj
@@ -1,15 +1,13 @@
 
 ; create struct
 (defstruct foo :speed :size)
-(defstruct hoo :vvv :aaa)
 (def ball (struct foo 2 5))
-(def vall (struct foo 1 4))
-(def hall (struct hoo 1 2))
 
 ; create an array to store the structure
 (def arr [])
 
 ; add struct to array
-(conj arr ball)
-(conj arr vall)
-(conj arr hall)
+(def arr (conj arr ball))
+(do arr)
+(def arr (conj arr ball))
+(do arr)
