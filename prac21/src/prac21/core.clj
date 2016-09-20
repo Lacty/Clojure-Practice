dc2e063179e67855c2400db8c56501d86578e82e
Lacty <akira206@gmail.com>
2016-09-20 23:07:47 +0900

    update

--- prac21/src/prac21/core.clj
+++ prac21/src/prac21/core.clj
@@ -1,27 +1,32 @@
 (ns prac21.core
   (:require [quil.core :refer :all]))
 
-(defstruct Ball :pos :size)
+(defstruct Ball :pos :size :color)
 
 (defn setup []
   (smooth)
   (no-stroke)
-  (frame-rate 10)
+  (frame-rate 30)
   (set-state! :balls (atom [])))
 
-(defn create-ball [pos size]
-  (struct Ball pos size))
+(defn create-ball [pos size color]
+  (struct Ball pos size color))
 
+(def low-color-val 50)
 (defn increase-ball [state]
   (swap! (state :balls) conj
-    (create-ball [50 50] 20)))
+    (create-ball [(rand 200.0) (rand 200.0)] ; pos
+                 (rand 40.0) ; size
+                 [(+ low-color-val (rand (- 256 low-color-val)))
+                  (+ low-color-val (rand (- 256 low-color-val)))
+                  (+ low-color-val (rand (- 256 low-color-val)))]))) ; color
 
 (defn draw-ball [ball]
   (let [[x y] (:pos ball)
-        size (:size ball)]
-    (do (fill 0) ; color
-        (ellipse x y size size)
-        (println "x = " size))))
+        size (:size ball)
+        [r g b] (:color ball)]
+    (do (fill r g b)
+        (ellipse x y size size))))
 
 (defn draw-balls [[ball & balls]]
   (when ball
